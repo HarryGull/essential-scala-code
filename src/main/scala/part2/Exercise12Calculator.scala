5a60c2e59d1b133fa9079d9b1980425d324f8d44
harrygull <15017045+HarryGull@users.noreply.github.com>
2021-04-12 11:00:29 +0100

    implement calculate

--- src/main/scala/part2/Exercise12Calculator.scala
+++ src/main/scala/part2/Exercise12Calculator.scala
@@ -1,20 +1,37 @@
 package part2
 
+//import part2.Expr.stringify
+
 // ----------------------------------------------
 
 // Step 1. Write a definition for Expr here!
+case class Add(first:Expr, second:Expr) extends Expr {
+  override def stringify = s"${first.stringify} + ${second.stringify}"
+}
+case class Subtraction(first:Expr, second:Expr) extends Expr{
+  override def stringify = s"$first - $second"
+}
+case class Mul(first:Expr, second:Expr) extends Expr{
+  override def stringify = s"$first x $second"
+}
+case class Division(first:Expr, second:Expr) extends Expr {
+  override def stringify = s"$first / $second"
+}
+case class SquareRoot(num:Expr) extends Expr {
+  override def stringify = s"√$num"
+}
+
+case class Num(num:Double) extends Expr {
+  override def stringify: String = num.toString
+  override def toString: String = stringify
+
+}
 
 sealed abstract class Expr {
-  def stringify(expr: Expr): String = {
-    expr match {
-      case Addition(num1, num2) => s"$num1 + $num2"
-      case Subtraction(num1, num2) => s"$num1 - $num2"
-      case Multiplication(num1, num2) => s"$num1 x $num2"
-      case Division(num1, num2) => s"$num1 / $num2"
-      case SquareRoot(num) => s"$num"
-    }
-  }
+  def stringify:String
 }
+
+
 
 // Handle the following types of equation:
 // - addition
@@ -51,6 +68,26 @@ object IntCalculator {
 // ----------------------------------------------
 
 object Expr {
+
+//  def stringify:String = this match {
+//    case Add(num1, num2) => s"$num1 + $num2"
+//          case Subtraction(num1, num2) => s"$num1 - $num2"
+//          case Mul(num1, num2) => s"$num1 x $num2"
+//          case Division(num1, num2) => s"$num1 / $num2"
+//          case SquareRoot(num) => s"√$num"
+//          case Num(num) => num.toString
+//  }
+
+//  def stringify: String = {
+//    this match {
+//      case Add(num1, num2) => s"$num1 + $num2"
+//      case Subtraction(num1, num2) => s"$num1 - $num2"
+//      case Mul(num1, num2) => s"$num1 x $num2"
+//      case Division(num1, num2) => s"$num1 / $num2"
+//      case SquareRoot(num) => s"√$num"
+//      case Num(num) => num.toString
+//    }
+//  }
   // def pythag(a: Double, b: Double): Expr = {
   //   ???
   // }
@@ -61,12 +98,13 @@ object Expr {
 }
 
 object Exercise11Calculator {
-  // val calc1 = Add(Num(1.1), Mul(Num(2.2), Num(3.3)))
+   val calc1: Add = Add(Num(1.1), Mul(Num(2.2), Num(3.3)))
+
   // val calc2 = Add(Mul(Num(1.1), Num(2.2)), Num(3.3))
 
   def main(args: Array[String]): Unit = {
     println("stringify")
-    // println(calc1.stringify)
+    println(calc1.stringify)
     // println(calc2.stringify)
 
     println("Calculator.eval")
